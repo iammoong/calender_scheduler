1f11c5f4946841cfad8276904aa14823e2988e46
iammoong <ansrlwkd225@naver.com>
2024-03-29 23:08:03 +0900

    ListView 활용 및 Bottom Sheet 만듬

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:calender_scheduler/component/calendar.dart';
 import 'package:calender_scheduler/component/schedule_card.dart';
 import 'package:calender_scheduler/component/today_banner.dart';
+import 'package:calender_scheduler/const/colors.dart';
+import 'package:calender_scheduler/schedule_bottom_sheet.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -21,6 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: renderFloatingActionButton(),
       body: SafeArea(
         child: Column(
           children: [
@@ -35,18 +38,26 @@ class _HomeScreenState extends State<HomeScreen> {
                 scheduleCount: 3,
             ),
             SizedBox(height: 8.0),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: ScheduleCard(
-                  startTime: 8,
-                  endTime: 9,
-                  content: "프로그래밍 공부",
-                  color: Colors.red),
-            )
+            _ScheduleList(),
           ],
         ),
       ),
     );
+  }
+
+  FloatingActionButton renderFloatingActionButton() {
+    return FloatingActionButton(
+      onPressed: () {
+        showModalBottomSheet(
+            context: context,
+            isScrollControlled: true,
+            builder: (_){
+              return ScheduleBottomSheet();
+            },
+        );
+      },
+      backgroundColor: PRIMARY_COLOR,
+      child: Icon(Icons.add),);
   }
 
   onDaySelected(DateTime selectedDay, DateTime focusedDay){
@@ -57,3 +68,30 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 }
+
+class _ScheduleList extends StatelessWidget {
+  const _ScheduleList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: ListView.separated(
+            itemCount: 5,
+            separatorBuilder: (context, index) {
+              return SizedBox(height: 8.0);
+            },
+            itemBuilder: (context, index){
+              return  ScheduleCard(
+                  startTime: 8,
+                  endTime: 9,
+                  content: "프로그래밍 공부",
+                  color: Colors.red);
+            }
+        ),
+      ),
+    );
+  }
+}
+
